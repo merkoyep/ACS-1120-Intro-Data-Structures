bf52a1622f61b41a5c892ddafdb396546fca1598
merkoyep <81783308+merkoyep@users.noreply.github.com>
2024-01-24 14:51:10 -0800

    added missing features to tutorial page 3

--- Code/histogram.py
+++ Code/histogram.py
@@ -5,7 +5,7 @@ with open("../Code/data/histogramtext.txt", 'r', encoding='utf-8') as file:
     words = [re.sub(r'[^a-zA-Z]', '', word) for line in file for word in line.split()]
     
 def histogram(text):
-    # takes text and returns a dictionary of each word and its occurence in the file.
+    # takes text and returns a dictionary of each word as a key and its occurence as values.
     histogram = {}
     for word in text:
         if word in histogram:
@@ -21,3 +21,19 @@ def unique_words(histogram):
 def frequency(word, histogram):
     # Takes a word, and histogram, returns times the word appears in the text
     return histogram[word]
+
+def average(words, histogram):
+    # Returns mean frequency of words in text
+    return len(words) / unique_words(histogram)
+
+def find_least_frequent_words(histogram):
+    # Returns list of least frequent words
+    lowest_frequency = min(histogram.values())
+    least_frequent_words = [key for key, value in histogram.items() if value == lowest_frequency]
+    return least_frequent_words
+
+def find_most_frequent_words(histogram):
+    # Returns list of most frequent words
+    highest_frequency = max(histogram.values())
+    most_frequent_words = [key for key, value, in histogram.items() if value == highest_frequency]
+    return most_frequent_words
